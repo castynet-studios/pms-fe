ac937f0a7b12de679c5cc774544be1effbbf399b
castynet <riungemaina@gmail.com>
2022-02-08 08:35:54 +0300

    index: bug fix

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
-import "antd/dist/antd.css";
+import "antd/dist/antd.min.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
