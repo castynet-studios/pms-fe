2296e77930a194da794b3b9941e53665820a25e9
Ana Muraya <Anamuraya9@gmail.com>
2022-07-28 02:36:28 +0300

    Added the rations card to the feeding page

--- src/components/events/eventsComp.jsx
+++ src/components/events/eventsComp.jsx
@@ -1,10 +1,10 @@
-import EventComp from 'components/event/eventComp'
+import cn from 'classnames'
 
 import styles from './eventsStyles.module.scss'
 
 const EventsComp = ({ events }) => {
   return (
-    <div>
+    <div className={styles.eventsWrapper}>
       <div className={styles.title}>
         <h3>Event</h3>
         <h3>Date</h3>
@@ -13,8 +13,46 @@ const EventsComp = ({ events }) => {
         <h3>Priority</h3>
       </div>
       <div className={styles.eventsList}>
-        {events.map((event, index) => (
-          <EventComp event={event} key={index} />
+        {events.map(event => (
+          <div key={event.id} className={styles.eventsection}>
+            <h3 className={cn(styles.event, styles.section)}>{event.event}</h3>
+            <div className={cn(styles.eventDate, styles.section)}>
+              {event.days !== 0 ? (
+                <div className={styles.dateDays}>
+                  <h3 className={styles.days}>{event.days} Days</h3>
+                  {event.date === 'Overdue' ? (
+                    <h3 className={styles.overdue}>{event.date}</h3>
+                  ) : (
+                    <h3 className={styles.date}>{event.date}</h3>
+                  )}
+                </div>
+              ) : (
+                <div className={styles.today}>
+                  <p>Today</p>
+                </div>
+              )}
+            </div>
+            <div className={cn(styles.section, styles.center)}>
+              {event.assigned === 'Multiple' ? (
+                <h3 className={styles.multiple}>{event.assigned}</h3>
+              ) : (
+                <h3 className={styles.assigned}>{event.assigned}</h3>
+              )}
+            </div>
+            <div className={cn(styles.section, styles.center)}>
+              {event.pig === 'Multiple' ? (
+                <h3 className={styles.multiple}>{event.pig}</h3>
+              ) : (
+                <h3 className={styles.pig}>{event.pig}</h3>
+              )}
+            </div>
+            <h3 className={cn(styles.section, styles.priority)}>
+              <div>{event.priority}</div>
+            </h3>
+            <div className={styles.pointers}>
+              <p>&gt;&gt;</p>
+            </div>
+          </div>
         ))}
       </div>
     </div>
---
+++ src/components/rations/Rations.jsx
@@ -0,0 +1,41 @@
+import styles from './style.module.scss'
+
+const RationButtons = ({ name }) => {
+  return (
+    <div className={styles.rationButton}>
+      <p>{name}</p> <p>&gt;&gt;</p>
+    </div>
+  )
+}
+
+const Rations = ({ rations }) => {
+  return (
+    <div className={styles.divWrapper}>
+      <div className={styles.heading}>RATIONS</div>
+      <div className={styles.headings}>
+        <div className={styles.tag}>Tag</div>
+        <div className={styles.ration}>Ration</div>
+        <div className={styles.weight}>Weight</div>
+        <div className={styles.nextFeedingTime}>Next Feeding Time</div>
+      </div>
+      <div className={styles.body}>
+        {rations.map(ration => (
+          <div className={styles.bodyInfo} key={ration.id}>
+            <div className={styles.tag}>{ration.tag}</div>
+            <div className={styles.ration}>{ration.ration}</div>
+            <div className={styles.weight}>{ration.weight}</div>
+            <div className={styles.nextFeedingTime}>
+              {ration.nextFeedingTime}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={styles.buttons}>
+        <RationButtons name="See all rations" />
+        <RationButtons name="Update rations" />
+      </div>
+    </div>
+  )
+}
+
+export default Rations
