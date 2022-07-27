0ccb5c0d7c4d9a87f9db1655c0d4f195ff4fa78f
Ana Muraya <Anamuraya9@gmail.com>
2022-07-27 23:28:22 +0300

    Update events page

--- src/pages/events/events.jsx
+++ src/pages/events/events.jsx
@@ -1,61 +1,9 @@
-import { useState } from 'react'
-
 import EventsComp from 'components/events/eventsComp'
+import events from '../../events.json'
 
 import styles from './events.module.scss'
 
 const Events = () => {
-  const [events] = useState([
-    {
-      event: 'Feeding',
-      days: '5 days',
-      date: 'Overdue',
-      assigned: 'Kevin',
-      pig: 'A001K',
-      priority: 'High',
-    },
-    {
-      event: 'Tagging',
-      days: '2 days',
-      date: '12/01/2022',
-      assigned: 'Melvin',
-      pig: 'A001K',
-      priority: 'Low',
-    },
-    {
-      event: 'Watering',
-      days: '',
-      date: 'Today',
-      assigned: 'Kevin',
-      pig: 'A001K',
-      priority: 'Crucial',
-    },
-    {
-      event: 'Feeding',
-      days: '5 days',
-      date: 'Overdue',
-      assigned: 'Kevin',
-      pig: 'A001K',
-      priority: 'High',
-    },
-    {
-      event: 'Tagging',
-      days: '2 days',
-      date: '12/01/2022',
-      assigned: 'Melvin',
-      pig: 'A001K',
-      priority: 'Low',
-    },
-    {
-      event: 'Watering',
-      days: '',
-      date: 'Today',
-      assigned: 'Kevin',
-      pig: 'A001K',
-      priority: 'Crucial',
-    },
-  ])
-
   return (
     <>
       <div className={styles.eventsWrapper}>
