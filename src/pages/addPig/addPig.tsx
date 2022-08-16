cd1105d28792d76121f1d24374ce5ca7c78624d7
Omafovbe I <i.omafovbe@gmail.com>
2022-08-16 08:48:01 +0100

    feat: :technologist: inserted form input IDs
    Added form input and label attributes of id and for so as to improve form accessibility

--- src/pages/addPig/addPig.tsx
+++ src/pages/addPig/addPig.tsx
@@ -14,30 +14,30 @@ const addNewPig = () => {
             <label htmlFor="tagNumber">TagNumber</label>
             <input type="text" className={styles.sizeInputs} id="tagNumber" />
 
-            <label className={styles.smallHeader}>Stage</label>
-            <input type="text" className={styles.sizeInputs} />
+            <label htmlFor="stage" className={styles.smallHeader}>Stage</label>
+            <input type="text" className={styles.sizeInputs} id="stage" />
 
-            <label className={styles.smallHeader}>Age</label>
-            <input type="number" className={styles.sizeInputs} />
+            <label htmlFor="pigAge" className={styles.smallHeader}>Age</label>
+            <input type="number" className={styles.sizeInputs} id="pigAge" />
 
-            <label className={styles.smallHeader}>Breed</label>
-            <input type="text" className={styles.sizeInputs} />
+            <label htmlFor="pigBreed" className={styles.smallHeader}>Breed</label>
+            <input type="text" className={styles.sizeInputs} id="pigBreed" />
           </div>
 
           <div>
             <h1 className={styles.heads}>Lineage</h1>
 
-            <label className={styles.smallHeader}>Mother</label>
-            <input type="text" className={styles.sizeInputs} />
+            <label htmlFor="pigMother" className={styles.smallHeader}>Mother</label>
+            <input type="text" className={styles.sizeInputs} id="pigMother" />
 
-            <label className={styles.smallHeader}>Father</label>
-            <input type="text" className={styles.sizeInputs} />
+            <label htmlFor="pigFather" className={styles.smallHeader}>Father</label>
+            <input type="text" className={styles.sizeInputs} id="pigFather"/>
 
-            <label className={styles.smallHeader}>Grandmother</label>
-            <input type="text" className={styles.sizeInputs} />
+            <label htmlFor="pigGrandMother" className={styles.smallHeader}>Grandmother</label>
+            <input type="text" className={styles.sizeInputs} id="pigGrandMother" />
 
-            <label className={styles.smallHeader}>Grandfather</label>
-            <input type="text" className={styles.sizeInputs} />
+            <label htmlFor="pigGrandFather" className={styles.smallHeader}>Grandfather</label>
+            <input type="text" className={styles.sizeInputs} id="pigGrandFather" />
           </div>
         </div>
         <div className={styles.line}></div>
@@ -46,47 +46,47 @@ const addNewPig = () => {
         <div className={styles.part2}>
           <h1 className={styles.heads}>Health</h1>
           <div className={styles.pigSection}>
-            <label className={styles.smallHeader}>Weight (KGs)</label>
-            <input type="number" className={styles.sizeInputs} />
+            <label htmlFor="weightKg" className={styles.smallHeader}>Weight (KGs)</label>
+            <input type="number" className={styles.sizeInputs} id="weightKg" />
 
             <div className={styles.checkBoxRows}>
-              <input type="checkbox" />
-              <label className="check">Clipped</label>
+              <input type="checkbox" id="clippedInfo" />
+              <label htmlFor="clippedInfo" className="check">Clipped</label>
 
-              <input type="checkbox" />
-              <label className="check"> Tagged</label>
+              <input type="checkbox" id="taggedInfo" />
+              <label htmlFor="taggedInfo" className="check"> Tagged</label>
 
-              <input type="checkbox" />
-              <label className="check"> Fully Vaccinated</label>
+              <input type="checkbox" id="vaccineInfo" />
+              <label htmlFor="vaccineInfo" className="check"> Fully Vaccinated</label>
             </div>
           </div>
 
           <div className={styles.pigDetails}>
-            <label className={styles.smallHeader}>Predisposed to:</label>
-            <input type="text" className={styles.sizeInputs} />
+            <label htmlFor="predisposedTo" className={styles.smallHeader}>Predisposed to:</label>
+            <input type="text" className={styles.sizeInputs} id="predisposedTo" />
 
-            <label className={styles.smallHeader}>Illnesses</label>
-            <input type="text" className={styles.sizeInputs} />
+            <label htmlFor="illnesses" className={styles.smallHeader}>Illnesses</label>
+            <input type="text" className={styles.sizeInputs} id="illnesses" />
 
-            <label className={styles.smallHeader}>Vaccinations</label>
-            <input type="text" className={styles.sizeInputs} />
+            <label htmlFor="vaccinations" className={styles.smallHeader}>Vaccinations</label>
+            <input type="text" className={styles.sizeInputs} id="vaccinations" />
 
-            <label className={styles.smallHeader}>Notes</label>
-            <input type="text" className={styles.sizeInputs} />
+            <label htmlFor="notes" className={styles.smallHeader}>Notes</label>
+            <input type="text" className={styles.sizeInputs} id="notes" />
           </div>
 
           <h1 className={styles.heads}>Origin</h1>
           <div className={styles.pigDetails}>
             <div className={styles.checkBoxRows}>
-              <input type="checkbox" />
-              <label className="check">Locally Bred</label>
+              <input type="checkbox" id="locallyBred" />
+              <label htmlFor="locallyBred" className="check">Locally Bred</label>
             </div>
 
-            <label className={styles.smallHeader}>Farm/Farmer’s Name</label>
-            <input type="text" className={styles.sizeInputs} />
+            <label htmlFor="farmName" className={styles.smallHeader}>Farm/Farmer’s Name</label>
+            <input type="text" className={styles.sizeInputs} id="farmName" />
 
-            <label className={styles.smallHeader}>Contact</label>
-            <input type="number" className={styles.sizeInputs} />
+            <label htmlFor="farmContact" className={styles.smallHeader}>Contact</label>
+            <input type="number" className={styles.sizeInputs} id="farmContact" />
           </div>
         </div>
       </div>
